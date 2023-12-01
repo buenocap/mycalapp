60d391836a68d21232dc39b1ccc9cdfb4585635d
Pedro Bueno <pedro.buenocastaneda@ucdenver.edu>
2023-12-01 09:40:32 -0700

    Space out form input bar

--- src/TaskInput.js
+++ src/TaskInput.js
@@ -18,28 +18,38 @@ export default function TaskInput(props) {
   }
 
   return (
-    <div className="task-input">
-      <form onSubmit={handleSubmit}>
-        <label>Task Name: </label>
+    <form onSubmit={handleSubmit} className="task-input">
+      <div>
+        <label htmlFor="taskname">Task Name: </label>
         <input
           type="text"
           value={props.task}
           onChange={(e) => props.setTask(e.target.value)}
+          id="taskaname"
         ></input>
-        <label>Due Date: </label>
+      </div>
+
+      <div>
+        <label htmlFor="duedate">Due Date: </label>
         <input
           type="date"
           value={props.taskDueDate}
           onChange={(e) => props.setTaskDueDate(e.target.value)}
+          id="duedate"
         ></input>
-        <label>Time Due: </label>
+      </div>
+
+      <div>
+        <label htmlFor="timedue">Time Due: </label>
         <input
           type="time"
           value={props.taskDueTime}
           onChange={(e) => props.setTaskDueTime(e.target.value)}
+          id="timedue"
         ></input>
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+      </div>
+
+      <button type="submit">Submit</button>
+    </form>
   );
 }
