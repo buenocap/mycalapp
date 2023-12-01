16e5c67bbe4895f3faf9cfb9afb58c2684d82254
Pedro Bueno <pedro.buenocastaneda@ucdenver.edu>
2023-12-01 10:03:23 -0700

    setting up tasklist and calendar

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,7 +1,12 @@
 export default function Calendar() {
   return (
     <div class="calendar calendar-container">
-      <p>Pokemon</p>
+      <div className="calendar-header">
+        <h3>Calendar</h3>
+      </div>
+      <div>
+        <p>Calendar Object</p>
+      </div>
     </div>
   );
 }
--- src/TaskItem.js
+++ src/TaskItem.js
@@ -1,6 +1,9 @@
 export default function TaskItem({ task }) {
   return (
     <div className="task-item">
+      <form>
+        <input type="checkbox" />
+      </form>
       {task.taskDate ? (
         <li>
           <h3>{task.taskName}</h3> Due {task.taskDate} at {task.taskTime}
